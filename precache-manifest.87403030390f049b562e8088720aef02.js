1e46e0cebb8305b446fda4e532e8969d32d11ddf
unknown <dragostinj@gmail.com>
2019-01-21 13:42:25 +0200

    Updates

---
+++ precache-manifest.87403030390f049b562e8088720aef02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ba6cc06f150d65a591b",
+    "url": "/video/static/js/runtime~main.8ba6cc06.js"
+  },
+  {
+    "revision": "bbc1695725296088ec95",
+    "url": "/video/static/js/main.bbc16957.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/video/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "bbc1695725296088ec95",
+    "url": "/video/static/css/main.80789965.chunk.css"
+  },
+  {
+    "revision": "17677c45833b9569e674c37e9a7a1dc3",
+    "url": "/video/index.html"
+  }
+];
